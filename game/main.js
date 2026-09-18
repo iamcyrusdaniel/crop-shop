16e2e2518f7b9c5fc122adaeac866aee2c7e8594
iamcyrusdaniel <iamcyrusdaniel@gmail.com>
2026-09-17 20:44:01 -0400

    Removed unnecessary comments

--- game/main.js
+++ game/main.js
@@ -9,20 +9,6 @@ var selected_crop_node = null
 var overlapped_slots = []
 var chosen_slot = null
 
-// test_crop.addEventListener("mousemove", (event) => {
-//   if (!dragging) {return}
-//   event.preventDefault();
-
-//   let computedStyles = window.getComputedStyle(test_crop);
-//   let left_pos = computedStyles.left
-//   let top_pos = computedStyles.top
-  
-//   test_crop.style.left = `${parseInt(left_pos)+(event.clientX-starting_mouse_posX)}px`;
-//   test_crop.style.top = `${parseInt(top_pos)+(event.clientY-starting_mouse_posY)}px`;
-//   // console.log(`${parseInt(left_pos)+(event.clientX-starting_mouse_posX)}px`);
-//   // console.log(`Coordinates: X=${`${parseInt(left_pos)+(event.clientX-starting_mouse_posX)}px;`}, Y=${parseInt(left_pos)}`);
-// })
-
 test_crop.addEventListener("mousedown", (event) => {
   dragging = true
 
@@ -44,18 +30,9 @@ document.addEventListener("mousemove", (e) => {
 
   const overlap_slots = getOverlappingElements(selected_crop_node)
 
-  // const crop_rect = selected_crop_node.getBoundingClientRect();
-  // const crop_cordsX = e.clientX
-  // const crop_cordsY = e.clientY
-
   if (overlap_slots.length > 0) {
     for (slot of overlap_slots) {
-      // slot.style.backgroundColor = "rgb(199, 199, 199)";
-      // overlapped_slots.push(slot)
       const slot_rect = slot.getBoundingClientRect();
-
-      // const distance = Math.hypot(crop_cordsX - slot_cordsX, crop_cordsY - slot_cordsY)
-
       const is_overlaping = e.clientX >= slot_rect.left && e.clientX <= slot_rect.right && e.clientY >= slot_rect.top && e.clientY <= slot_rect.bottom;
 
       if (is_overlaping) {
@@ -68,45 +45,6 @@ document.addEventListener("mousemove", (e) => {
      }
     }
   }
-  // for (slot of overlapped_slots) {
-  //   if (!overlap_slots.includes(slot)) {
-  //     slot.style.backgroundColor = "rgb(255, 255, 255)";
-  //     overlapped_slots.splice(overlapped_slots.indexOf(slot), 1)
-  //   }
-  // }
-  // if (overlapped_slot && overlapped_slot != closest_element) {overlapped_slot.style.backgroundColor = "rgb(255,255,255)"}
-  // overlapped_slot = closest_element
-  // if (overlapped_slot) {
-  //   overlapped_slot.style.backgroundColor = "rgb(199, 199, 199)"
-  // }
-
-  // const play_grid = document.querySelector(".placement-grid")
-  // let shortest_distance = 100000
-  // let closest_element = null
-
-  // const crop_rect = test_crop.getBoundingClientRect();
-  // const crop_cordsX = crop_rect.left
-  // const crop_cordsY = crop_rect.top
-
-  // for (const slot of play_grid.children) {
-  //   const slot_rect = slot.getBoundingClientRect();
-  //   const slot_cordsX = slot_rect.left //- slot_rect.width
-  //   const slot_cordsY = slot_rect.top //- slot_rect.height
-
-  //   const distance = Math.hypot(crop_cordsX - slot_cordsX, crop_cordsY - slot_cordsY)
-  //   if (shortest_distance > distance) {
-  //     console.log(distance)
-  //     shortest_distance = distance
-  //     closest_element = slot
-  //   }
-  // }
-  // if (closest_element != null) {
-  //   if (overlapped_slot != null) {
-  //     overlapped_slot.style.backgroundColor = "rgb(255, 255, 255)"
-  //   }
-  //   overlapped_slot = closest_element
-  //   overlapped_slot.style.backgroundColor = "rgb(199, 199, 199)"
-  // }
 });
 
 document.addEventListener("mouseup", () => {
